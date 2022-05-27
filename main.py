ed0f22b97bdca7e433005cbc8643946a34546827
greysoh <greysoh@pm.me>
2022-05-26 22:34:57 -0400

    fix: Bruh.

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import os
 
 class Plugin:
     async def _main(self):
-        os.system(os.getenv("HOME") + "/homebrew/plugins/SpiceDeck/SpiceDeckServer")git clone -b main https://github.com/Wolvan/spdck.git /home/deck/homebrew/plugins/spdck
+        os.system(os.getenv("HOME") + "/homebrew/plugins/SpiceDeck/SpiceDeckServer")
